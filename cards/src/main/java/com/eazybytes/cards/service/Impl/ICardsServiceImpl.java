45a84ec2121c54118805a200cfdab623647dcdfb
MOHIT KUMAR SHAW <mohitshawmath@gmail.com>
2025-02-14 01:03:40 +0530

    SYS-004

--- cards/src/main/java/com/eazybytes/cards/service/Impl/ICardsServiceImpl.java
+++ cards/src/main/java/com/eazybytes/cards/service/Impl/ICardsServiceImpl.java
@@ -1,4 +1,26 @@
 package com.eazybytes.cards.service.Impl;
 
-public class ICardsServiceImpl {
+import com.eazybytes.cards.dto.CardsDto;
+import com.eazybytes.cards.service.ICardsService;
+
+public class ICardsServiceImpl implements ICardsService {
+    @Override
+    public void createCard(String mobileNumber) {
+
+    }
+
+    @Override
+    public CardsDto fetchCard(String mobileNumber) {
+        return null;
+    }
+
+    @Override
+    public boolean updateCard(CardsDto cardsDto) {
+        return false;
+    }
+
+    @Override
+    public boolean deleteCard(String mobileNumber) {
+        return false;
+    }
 }
