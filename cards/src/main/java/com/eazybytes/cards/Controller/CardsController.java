27c5069a4af1bf1a2fb19b43ceb1fc67b3429a36
MOHIT KUMAR SHAW <mohitshawmath@gmail.com>
2025-02-14 01:49:49 +0530

    SYS-006

--- cards/src/main/java/com/eazybytes/cards/Controller/CardsController.java
+++ cards/src/main/java/com/eazybytes/cards/Controller/CardsController.java
@@ -1,12 +1,25 @@
 package com.eazybytes.cards.Controller;
 
+import com.eazybytes.cards.constants.CardConstants;
+import com.eazybytes.cards.dto.CardsDto;
+import com.eazybytes.cards.dto.ResponseDto;
 import com.eazybytes.cards.service.ICardsService;
+import io.swagger.v3.oas.annotations.Operation;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,13 +29,64 @@ import org.springframework.web.bind.annotation.RestController;
 public class CardsController {
 
     @Autowired
-    private ICardsService cardsService;
+    private ICardsService iCardsService;
 
     @GetMapping("/health")
     public String Health() {
         return "Cards Service is up and running";
     }
 
+    @Operation(summary = "Create card", description = "Create a new card for a given mobile number", tags = {"Cards"})
+    @PostMapping("/create")
+    public ResponseEntity<ResponseDto> createCard(@Valid @RequestParam
+                                                  @Pattern(regexp = "(^$|[0-9]{10})", message = "Mobile number must be 10 digits")
+                                                  String mobileNumber) {
+        iCardsService.createCard(mobileNumber);
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(new ResponseDto(CardConstants.STATUS_201, CardConstants.MESSAGE_201));
+    }
+
+    @Operation(summary = "Fetch card details", description = "Fetch card details for a given mobile number", tags = {"Cards"})
+    @GetMapping("/fetch")
+    public ResponseEntity<CardsDto> fetchCardDetails(@RequestParam
+                                                     @Pattern(regexp = "(^$|[0-9]{10})", message = "Mobile number must be 10 digits")
+                                                     String mobileNumber) {
+        CardsDto cardsDto = iCardsService.fetchCard(mobileNumber);
+        return ResponseEntity.status(HttpStatus.OK).body(cardsDto);
+    }
+
+    @Operation(summary = "Update card details", description = "Update card details for a given mobile number", tags = {"Cards"})
+    @PutMapping("/update")
+    public ResponseEntity<ResponseDto> updateCardDetails(@Valid @RequestBody CardsDto cardsDto) {
+        boolean isUpdated = iCardsService.updateCard(cardsDto);
+        if (isUpdated) {
+            return ResponseEntity
+                    .status(HttpStatus.OK)
+                    .body(new ResponseDto(CardConstants.STATUS_200, CardConstants.MESSAGE_200));
+        } else {
+            return ResponseEntity
+                    .status(HttpStatus.EXPECTATION_FAILED)
+                    .body(new ResponseDto(CardConstants.STATUS_417, CardConstants.MESSAGE_417_UPDATE));
+        }
+    }
+
+    @Operation(summary = "Delete card details", description = "Delete card details for a given mobile number", tags = {"Cards"})
+    @DeleteMapping("/delete")
+    public ResponseEntity<ResponseDto> deleteCardDetails(@RequestParam
+                                                         @Pattern(regexp = "(^$|[0-9]{10})", message = "Mobile number must be 10 digits")
+                                                         String mobileNumber) {
+        boolean isDeleted = iCardsService.deleteCard(mobileNumber);
+        if (isDeleted) {
+            return ResponseEntity
+                    .status(HttpStatus.OK)
+                    .body(new ResponseDto(CardConstants.STATUS_200, CardConstants.MESSAGE_200));
+        } else {
+            return ResponseEntity
+                    .status(HttpStatus.EXPECTATION_FAILED)
+                    .body(new ResponseDto(CardConstants.STATUS_417, CardConstants.MESSAGE_417_DELETE));
+        }
+    }
 
 
 }
--- cards/src/main/java/com/eazybytes/cards/service/Impl/ICardsServiceImpl.java
+++ cards/src/main/java/com/eazybytes/cards/service/Impl/ICardsServiceImpl.java
@@ -1,30 +1,79 @@
 package com.eazybytes.cards.service.Impl;
 
+import com.eazybytes.cards.constants.CardConstants;
 import com.eazybytes.cards.dto.CardsDto;
+import com.eazybytes.cards.entity.Cards;
+import com.eazybytes.cards.exceptions.CardAlreadyExistsException;
+import com.eazybytes.cards.exceptions.ResourceNotFoundException;
+import com.eazybytes.cards.mapper.CardsMapper;
+import com.eazybytes.cards.repository.CardsRepository;
 import com.eazybytes.cards.service.ICardsService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+import java.util.Random;
+
 @Service
 @AllArgsConstructor
 public class ICardsServiceImpl implements ICardsService {
+
+    private CardsRepository cardsRepository;
+
+    /**
+     * @param mobileNumber - Mobile Number of the Customer
+     */
+
     @Override
     public void createCard(String mobileNumber) {
-
+        Optional<Cards> optionalCards = cardsRepository.findByMobileNumber(mobileNumber);
+        if (optionalCards.isPresent()) {
+            throw new CardAlreadyExistsException("Card already registered with given mobileNumber " + mobileNumber);
+        }
+        cardsRepository.save(createNewCard(mobileNumber));
     }
 
+    /**
+     * @param mobileNumber - Mobile Number of the Customer
+     * @return the new card details
+     */
+    private Cards createNewCard(String mobileNumber) {
+        Cards newCard = new Cards();
+        long randomCardNumber = 100000000000L + new Random().nextInt(900000000);
+        newCard.setCardNumber(Long.toString(randomCardNumber));
+        newCard.setMobileNumber(mobileNumber);
+        newCard.setCardType(CardConstants.CREDIT_CARD);
+        newCard.setTotalLimit(CardConstants.NEW_CARD_LIMIT);
+        newCard.setAmountUsed(0);
+        newCard.setAvailableAmount(CardConstants.NEW_CARD_LIMIT);
+        return newCard;
+    }
+
+    /**
+     * @param mobileNumber - Input mobile Number
+     * @return
+     */
     @Override
     public CardsDto fetchCard(String mobileNumber) {
-        return null;
+        Optional<Cards> cards = cardsRepository.findByMobileNumber(mobileNumber);
+        if (!cards.isPresent()) {
+            throw new ResourceNotFoundException("Card", "mobileNumber", mobileNumber);
+        }
+        return CardsMapper.mapToCardsDto(cards.get(), new CardsDto());
     }
 
     @Override
     public boolean updateCard(CardsDto cardsDto) {
-        return false;
+        Cards cards = cardsRepository.findByCardNumber(cardsDto.getCardNumber()).orElseThrow(() -> new ResourceNotFoundException("Card", "CardNumber", cardsDto.getCardNumber()));
+        CardsMapper.mapToCards(cardsDto, cards);
+        cardsRepository.save(cards);
+        return true;
     }
 
     @Override
     public boolean deleteCard(String mobileNumber) {
-        return false;
+        Cards cards = cardsRepository.findByMobileNumber(mobileNumber).orElseThrow(() -> new ResourceNotFoundException("Card", "mobileNumber", mobileNumber));
+        cardsRepository.deleteById(cards.getCardId());
+        return true;
     }
 }
